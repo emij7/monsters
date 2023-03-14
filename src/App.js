8ebea2209fd100f8301aa06983e92d45bc6385b3
Ignacio Emiliano Juarez <emij_13@hotmail.com>
2023-03-14 13:53:05 -0300

    feat: show some monsters names

--- src/App.js
+++ src/App.js
@@ -1,34 +1,19 @@
 import { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      name: { firstName: "Emi", lastName: "Juarez" },
-      nacionalidad: "Argentino",
+      monsters: [{ name: "Ruperta" }, { name: "Jacky" }, { name: "Frank" }],
     };
   }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Hola {this.state.name.firstName} {this.state.name.lastName}, soy{" "}
-            {this.state.nacionalidad ?? "Argentino"}
-          </p>
-          <button
-            onClick={() => {
-              this.setState({
-                name: { firstName: "Emilio" },
-              });
-            }}
-          >
-            Change name
-          </button>
-        </header>
+        {this.state.monsters.map((monster, index) => {
+          return <h1 key={index}>{monster.name}</h1>;
+        })}
       </div>
     );
   }
