43a9980eafe2679c0b6d007eefc032bcac9f013c
Ignacio Emiliano Juarez <emij_13@hotmail.com>
2023-03-15 07:56:03 -0300

    feat: filter monster list

--- src/App.js
+++ src/App.js
@@ -5,13 +5,39 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      monsters: [{ name: "Ruperta" }, { name: "Jacky" }, { name: "Frank" }],
+      monsters: [],
+      filteredMonsters: [],
     };
   }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) =>
+        this.setState(() => {
+          return { monsters: users, filteredMonsters: users };
+        })
+      );
+  }
+
   render() {
     return (
       <div className="App">
-        {this.state.monsters.map((monster, index) => {
+        <input
+          className="search-box"
+          type="search"
+          placeholder="Search monsters"
+          onChange={(event) => {
+            const searchString = event.target.value.toLowerCase();
+            const filteredMonsters = this.state.monsters.filter((monster) => {
+              return monster.name.toLowerCase().includes(searchString);
+            });
+            this.setState(() => {
+              return { filteredMonsters: filteredMonsters };
+            });
+          }}
+        />
+        {this.state.filteredMonsters.map((monster, index) => {
           return <h1 key={index}>{monster.name}</h1>;
         })}
       </div>
