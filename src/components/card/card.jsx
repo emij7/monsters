9d65f1bdf94ced4836ee371da40d8d567ea7827b
Ignacio Emiliano Juarez <emij_13@hotmail.com>
2023-03-17 17:30:12 -0300

    fix: images path fixed

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -4,7 +4,7 @@ export const Card = ({ monster: { id, name, email } }) => (
   //Another way of destructuring, maybe better because is easier to read is:
   //const {id,name,email} = monster
   <div className="card-container">
-    <img src={`https:robohash.org/${id}?set=set2`} alt={`monster ${name}`} />
+    <img src={`https://robohash.org/${id}?set=set2`} alt={`monster ${name}`} />
     <h2>{name.toUpperCase()}</h2>
     <p className="card-email">{email}</p>
   </div>
