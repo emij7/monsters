f8800f1f55fc0d7a48b2375935c56cf7a095b987
Ignacio Emiliano Juarez <emij_13@hotmail.com>
2023-03-17 09:13:08 -0300

    feat: section useEffect added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import { useEffect, useState } from "react";
 import "./App.css";
-import CardList from "./components/cardList/cardList";
-import SearchBox from "./components/search-box/searchBox";
+import { SearchBox } from "./components/search-box/searchBox";
+import { CardList } from "./components/cardList/cardList";
 
 const App = () => {
   const [monsters, setMonsters] = useState([]);
   const [searchField, setSearchField] = useState("");
+  const [filteredMonsters, setFilteredMonsters] = useState(monsters);
 
   useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/users")
@@ -13,13 +14,18 @@ const App = () => {
       .then((users) => setMonsters(users));
   }, []);
 
-  const filteredMonsters = monsters.filter((monster) => {
-    return monster.name.toLowerCase().includes(searchField);
-  });
+  useEffect(() => {
+    const newFilteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLowerCase().includes(searchField);
+    });
+    setFilteredMonsters(newFilteredMonsters);
+  }, [monsters, searchField]);
+
   const onSearchChange = (event) => {
     const searchField = event.target.value.toLowerCase();
     setSearchField(searchField);
   };
+
   return (
     <div className="App">
       <h1 className="app-title">Monster Finder</h1>
