188ced5fdf387d0046ca1aea0f43530277554999
Ignacio Emiliano Juarez <emij_13@hotmail.com>
2023-03-17 13:04:09 -0300

    fix: map key attribute added

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -3,7 +3,7 @@ import "./card.styles.css";
 export const Card = ({ monster: { id, name, email } }) => (
   //Another way of destructuring, maybe better because is easier to read is:
   //const {id,name,email} = monster
-  <div className="card-container" id={id}>
+  <div className="card-container">
     <img src={`https:robohash.org/${id}?set=set2`} alt={`monster ${name}`} />
     <h2>{name.toUpperCase()}</h2>
     <p className="card-email">{email}</p>
--- src/components/cardList/cardList.jsx
+++ src/components/cardList/cardList.jsx
@@ -4,7 +4,7 @@ import "./cardList.styles.css";
 export const CardList = ({ monsters }) => (
   <div className="card-list">
     {monsters.map((monster) => {
-      return <Card monster={monster} />;
+      return <Card key={monster.id} monster={monster} />;
     })}
   </div>
 );
